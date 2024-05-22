c91eac754358f93243cb3f6f149b95fd400cc444
Desenvolvimento <desenvolvimento@zaal.com.br>
2024-05-21 23:41:56 -0300

    feat(entity): create getters

---
+++ solid/src/domain/product/entity/product.entity.ts
@@ -0,0 +1,40 @@
+/* eslint-disable no-useless-constructor */
+export type TProductProps = {
+  id: string
+  name: string
+  price: number
+  quantity: number
+}
+
+export class Product {
+  private constructor(private props: TProductProps) {}
+
+  public static create(name: string, price: number) {
+    return new Product({
+      id: crypto.randomUUID().toString(),
+      name,
+      price,
+      quantity: 0,
+    })
+  }
+
+  public static with(props: TProductProps) {
+    return new Product(props)
+  }
+
+  public get id() {
+    return this.props.id
+  }
+
+  public get name() {
+    return this.props.name
+  }
+
+  public get price() {
+    return this.props.price
+  }
+
+  public get quantity() {
+    return this.props.quantity
+  }
+}
